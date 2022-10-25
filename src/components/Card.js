1237739db6154e6d47fce0c41e3e5dff7251248c
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2022-10-25 18:09:23 +0100

    created reusable Card component

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Card({ children }) {
+  return (
+    <Box
+      bg="white"
+      boxShadow="lg"
+      p={10}
+      pb="60px"
+      borderRadius="md"
+      border="2px solid teal.800"
+    >
+      {children}
+    </Box>
+  );
+}
