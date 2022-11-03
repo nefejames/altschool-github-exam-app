8352b1ed9237d999da24f71e6a78fb83a43ece40
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2022-11-03 04:07:43 +0000

    fixed repodetail error and implementation

--- src/components/RepoDetails.js
+++ src/components/RepoDetails.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   Flex,
   Heading,
@@ -8,27 +9,28 @@ import {
   Spinner,
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import { useGitHubReposContext } from "../context/GitHubReposContext";
 import { FiExternalLink } from "react-icons/fi";
 
 export default function RepoDetails() {
   const { repoId } = useParams();
-  const data = useGitHubReposContext();
+  const [repoData, setRepoData] = useState();
+  const [loading, setLoading] = useState(false);
 
-  function getMatchingRepo() {
-    const matchingRepo = data.filteredRepos.find(
-      (repo) => repo.name === repoId
+  async function fetchRepoData() {
+    setLoading(true);
+    const response = await fetch(
+      `https://api.github.com/repos/nefejames/${repoId}`
     );
-    return matchingRepo;
+    const data = await response.json();
+    setRepoData(data);
+    setLoading(false);
   }
 
-  console.log(getMatchingRepo());
-  console.log(repoId);
-  console.log(data.filteredRepos);
+  useEffect(() => {
+    fetchRepoData();
+  }, []);
 
-  const repo = getMatchingRepo();
-
-  if (data.loading) return <Spinner size="xl" />;
+  if (loading || !repoData) return <Spinner size="xl" />;
 
   return (
     <Flex
@@ -45,25 +47,31 @@ export default function RepoDetails() {
         <VStack spacing={2} alignItems="start" w="full">
           <Flex justify="space-between" w="full">
             <Heading as="h2" fontSize="lg">
-              {repo.name}
+              {repoData.name}
             </Heading>
-            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+            <a
+              href={repoData.html_url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FiExternalLink />
             </a>
           </Flex>
 
-          {repo.description && (
+          {repoData.description && (
             <Text as="p" noOfLines={1}>
-              {repo.description}
+              {repoData.description}
             </Text>
           )}
         </VStack>
 
         <HStack spacing={3}>
-          {repo.language && <Tag colorScheme="green">{repo.language}</Tag>}
+          {repoData.language && (
+            <Tag colorScheme="green">{repoData.language}</Tag>
+          )}
           <Tag colorScheme="green">
-            {repo.stargazers_count > 0
-              ? `Stars: ${repo.stargazers_count}`
+            {repoData.stargazers_count > 0
+              ? `Stars: ${repoData.stargazers_count}`
               : "No Stars"}
           </Tag>
         </HStack>
